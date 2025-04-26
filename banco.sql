c252a029abfe7709f17a776da0d3148d73638cca
alin332r <alinebrandao332@gmail.com>
2025-04-25 22:34:51 -0300

    Microserviços com Docker

---
+++ banco.sql
@@ -0,0 +1,8 @@
+CREATE TABLE registros (
+     UsuarioID int,
+     NomeCompleto varchar(50),
+     Cargo varchar(50),
+     Departamento varchar(150),
+     Localidade varchar(50),
+     Servidor varchar(50)
+ );
